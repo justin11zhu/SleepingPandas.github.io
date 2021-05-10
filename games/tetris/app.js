f55a0d7fc364f7d80896cf2d3331fef9f22825cd
justindood <46207052+justindood@users.noreply.github.com>
2021-05-10 13:19:57 -0700

    Tetris Blocks Code
    Added some javascript stuff but not much functionality yet

---
+++ games/tetris/app.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const GRID_WIDTH = 1
+
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const scoreDisplay = document.querySelector('#score')
+    const startButton = document.querySelector('start-button')
+    const width = 10
+
+
+    console.log(squares)
+
+
+    //Tetris block code
+    const lBlock = [
+        [1, width+1, width*2+1, 2], 
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+
+    const zBlock = [
+        [width, width+1, width*2+1, width*2+2],
+        [2, width+1, width+2, width*2+1],
+        [width, width+1, width*2+1, width*2+2],
+        [2, width+1, width+2, width*2+1]
+    ]
+
+    const sBlock = [
+        [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
+        [GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1],
+        [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
+        [GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1]
+      ]
+    
+      const tBlock = [
+        [1, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2],
+        [1, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 1],
+        [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 1],
+        [1, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1]
+      ]
+    
+      const oBlock = [
+        [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
+        [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
+        [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
+        [0, 1, GRID_WIDTH, GRID_WIDTH + 1]
+      ]
+    
+      const iBlock = [
+        [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 3 + 1],
+        [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3],
+        [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 3 + 1],
+        [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3]
+      ]
+
+      const theBlocks = [lBlock, zBlock, sBlock, tBlock, oBlock, iBlock]
+
+      let currentPosition = 4
+      let current = theBlocks[0][0]
+
+
+      function draw(){
+          current.forEach(index => {
+              squares[currentPosition + index].classList.add('block')
+          })
+      }
+      
+      draw()
+
+})
